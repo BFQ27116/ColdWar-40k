c6580873d268fc29520bbd977101c092e9a68cba
Mario De Juan Gómez <BFQ27116@educastur.es>
2025-02-05 21:15:53 +0100

    Intente solucionar un problema de las carpetas

---
+++ ColdWar40k/src/ui/UI.java
@@ -0,0 +1,32 @@
+package ui;
+
+public class UI {
+
+	private static final String STARTER_MENU = null;
+	public static final Readable PLAY_OR_LOAD = null;
+
+	public static void showMenu() {
+		System.out.println(STARTER_MENU);
+	}
+
+	public static void showRules() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void showCredits() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void showRanking() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void gonnaPlayMenu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
